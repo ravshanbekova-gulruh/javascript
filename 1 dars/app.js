59d34ecc287790fdb593adf9b6e48e44d1a6fa92
alisher-toxirov <alishertoxirov1314@gmail.com>
2026-01-23 17:35:35 +0500

    js yozdim

---
+++ 1 dars/app.js
@@ -0,0 +1,23 @@
+// External JS file
+// console.log ("this is from app.js file")
+
+// //foydalanuvchidan ma'lumot olish
+// // prompt("ismimgizni kiriting") //string
+// // confirm("siz ro'yhatdan o'tishni xoxlaysizmi ")//boolean
+
+// // foydalanuvchiga ma'lumot berish 
+// // alert("xush kelibsiz")
+// // console.log("bu konsolga chiqarilgan ma'lumot")
+
+// // o'zgaruvchilar
+// let fname = 'name'
+// fname = 'new name' //o'zgaruvchilar qiymatini o'zgartirish mumkin
+
+// const birthYear = 1990 //o'zgaruvchi qiymatini o'zgartirish mumlin emas
+
+// let userName = prompt ("ismingizni kiriting") //camelCase
+// alert(userName)
+// let userAge = prompt("yoshingizni kiriting") // 2026 - userAge
+// alert('sizning ismingiz' + userName + ', yoshingiz' + userAge + 'da.')
+// // let userBirthYear = 2026 - userAge
+// alert('siz' + 2026 - userAge + 'yilda tugilgansiz')
---
+++ 2 dars/app.js
@@ -0,0 +1,22 @@
+// node -v
+// node app.js
+// node --watch app.js
+
+console.log('test')
+console.log('test 123')
+console.log('final test')
+
+// promt,alert,confirm - node js da ishlamaydi
+
+// --- Added Hello World function ---
+
+function helloWorld() {
+    console.log ('hello,world')
+}
+
+//call the function to execute it
+helloWorld()
+
+// --- end of added hello world function ---
+let userAge = 25;
+let userName = 'john doe'
