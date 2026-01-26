19c404f485f5cd52dd4a52084cda61571f155f08
alisher <alishertoxirov1314@gmail.com>
2026-01-26 17:48:36 +0500

    js yozdim

---
+++ 3 dars/app.js
@@ -0,0 +1,23 @@
+//data type
+
+//primitive data types - oddiy sodda ma'lumot turlari
+//1 string - yozuv
+let text = "string text"
+console .log (text);
+
+//templete literal
+let userName = "gulrukh"
+let userAge = 15;
+console.log("foydalanuvchining ismi:" + userName + ",yoshi:" + userAge)
+console.log(`foydalanuvchining ismi: ${userName}, yoshi: ${userAge}`);
+
+//2 number -son
+//3 boolean - mantiqiy
+//4 null - mavjud emas
+//5 undefined - aniqlanmagan
+//6 bigint - katta sonlar
+//7 symbol - maxsus qiymat
+
+//non-primitive data types - murakkab ma'lumot turlari
+// 1 object -> array, function
+
